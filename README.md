1082e3725a410b5effcf45820108d67ef9c35041
sevdas <sshukrieva@gmail.com>
2022-02-27 20:36:24 +0000

    get username data, send API request to GitHub

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ This tutorial covers:
 - Using jQuery AJAX functionality
 
 ## Goal
-- Built a webpage that can retrieve information from a GitHub user.
+- Built a webpage that can retrieve information from a GitHub user showing their username, information and picture.
+When the user has typed in the username, they should be able to trigger the API call to GitHub by pressing <enter>.
--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// github user finder example
+/* Pass the username to GitHub */
+$(document).ready(function() {
+  $(document).on('keypress', '#username', function(event) {
+    if (event.which === 13) { // check the key was <enter>
+      const input = $(this)
+      const username = input.val()
+
+      console.log(`Username was: ${username}`)
+    }
+  });
+});
+
+/* Pass this username through to GitHub */
+function getGithubInfo(username) {
+   const url = `https://api.github.com/users/${username}`
+
+   const xmlhttp = new XMLHttpRequest();
+   xmlhttp.open('GET', url, false)
+   xmlhttp.send()
+
+   const data = xmlhttp.responseText;
+   console.log(data)
+}
+
+
+
+
+
+
